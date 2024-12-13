626f202f13e493277b021b566dd7cafb563e2c8e
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-12-13 21:19:14 +0530

    alignment not proper , react , web crashed

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import "../global.css";
-import "../styleguide.css";
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import { DesktopLandingPage } from "./screens/DesktopLandingPage";
-
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<DesktopLandingPage />);
